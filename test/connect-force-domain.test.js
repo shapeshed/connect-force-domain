e7ca798d745241f8a4d6d87ef4f79f5bc18c161e
George Ornbo <george@shapeshed.com>
2012-03-07 09:24:14 +0000

    switch tests to mocha

--- test/connect-force-domain.test.js
+++ test/connect-force-domain.test.js
@@ -1,35 +1,63 @@
+var connect = require('connect')
+  , http = require('http')
+  , assert = require('assert')
+  , forceDomain = require('../lib/connect-force-domain');
 
-var connect = require('connect'),
-	force_domain = require('../lib/connect-force-domain'),
-	assert = require('assert');
+var app = connect()
+  .use(forceDomain('shapeshed.com'))
 
-app = connect.createServer(force_domain('shapeshed.com'));
-
-
-module.exports = {
-  'GET /': function(){
-    assert.response(app,
-      { url: '/' },
-      { status: 301 , headers: { 'Location': 'http://shapeshed.com/' }});
-  },
-  'GET /foobar': function(){
-    assert.response(app,
-      { url: '/foobar' },
-      { status: 301 , headers: { 'Location': 'http://shapeshed.com/foobar' }});
-  },
-  'GET /foobar/': function(){
-    assert.response(app,
-      { url: '/foobar/' },
-      { status: 301 , headers: { 'Location': 'http://shapeshed.com/foobar/' }});
-  },
-  'GET /two/folders': function(){
-    assert.response(app,
-      { url: '/two/folders' },
-      { status: 301 , headers: { 'Location': 'http://shapeshed.com/two/folders' }});
-  },
-  'GET /two/folders/': function(){
-    assert.response(app,
-      { url: '/two/folders/' },
-      { status: 301 , headers: { 'Location': 'http://shapeshed.com/two/folders/' }});
-  }
-};
+describe('Connect Force Domain module', function(){
+  before(function(){
+    http.Server(app).listen(3000);
+  })
+  describe('making a get request', function(){
+    it('should get return a 301 response', function(done){
+      http.get( { host: '127.0.0.1', port: '3000' }, function(res){
+        assert.strictEqual(res.statusCode, 301);
+        done();
+      })
+    })
+    it('should set the location header correctly', function(done){
+      http.get( { host: '127.0.0.1', port: '3000' }, function(res){
+        assert.strictEqual(res.headers['location'], 'http://shapeshed.com/')
+        done();
+      })
+    })
+    it('should include a path in the location header', function(done){
+      http.get( { host: '127.0.0.1', port: '3000', path: '/foobar' }, function(res){
+        assert.strictEqual(res.headers['location'], 'http://shapeshed.com/foobar')
+        done();
+      })
+    })
+    it('should include trailing slashes in location header', function(done){
+      http.get( { host: '127.0.0.1', port: '3000', path: '/foobar/' }, function(res){
+        assert.strictEqual(res.headers['location'], 'http://shapeshed.com/foobar/')
+        done();
+      })
+    })
+    it('should include nested routes in location header', function(done){
+      http.get( { host: '127.0.0.1', port: '3000', path: '/foo/bar' }, function(res){
+        assert.strictEqual(res.headers['location'], 'http://shapeshed.com/foo/bar')
+        done();
+      })
+    })
+    it('should include nested routes with trailing slashes in location header', function(done){
+      http.get( { host: '127.0.0.1', port: '3000', path: '/foo/bar/' }, function(res){
+        assert.strictEqual(res.headers['location'], 'http://shapeshed.com/foo/bar/')
+        done();
+      })
+    })
+    it('should include a query string in the location header', function(done){
+      http.get( { host: '127.0.0.1', port: '3000', path: '/foo?q=1' }, function(res){
+        assert.strictEqual(res.headers['location'], 'http://shapeshed.com/foo?q=1')
+        done();
+      })
+    })
+    it('should include a query string after a trailing slash in the location header', function(done){
+      http.get( { host: '127.0.0.1', port: '3000', path: '/foo/?q=1' }, function(res){
+        assert.strictEqual(res.headers['location'], 'http://shapeshed.com/foo/?q=1')
+        done();
+      })
+    })
+  })
+})
